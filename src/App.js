daa5052038ad10f3662e7bbbe39276183e99cfa5
amjdp <athiramj123@gmail.com>
2023-03-05 18:44:39 +0530

    login component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/login/Login';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Lets build the LinkedIn-clone 🚀 ! </h2> 
+      {/* <h2>Lets build the LinkedIn-clone 🚀 ! </h2>  */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login/>}> </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
