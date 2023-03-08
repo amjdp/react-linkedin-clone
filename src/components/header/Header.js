7d45f891dbeba2aeccbb316cb81eb6658482c6b4
amjdp <athiramj123@gmail.com>
2023-03-08 22:29:06 +0530

    navlist-home

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,44 +1,54 @@
 import styled from "styled-components";
 
 const Header = (props) => {
-    return (
-        <Container>
-            <Content>
-                <Logo>
-                    <a href="/home">
-                        <img src="/images/home-logo.svg" alt="home-logo" />
-                    </a>
-                </Logo>
-                <Search>
-                    <div>
-                        <input type="text" placeholder="Search" />
-                    </div>
-                    <SearchIcon>
-                    <img src="/images/search-icon.svg" alt="home-logo" />
-                    </SearchIcon>
-                </Search>
-            </Content>
-        </Container>
-    );
+  return (
+    <Container>
+      <Content>
+        <Logo>
+          <a href="/home">
+            <img src="/images/home-logo.svg" alt="home-logo" />
+          </a>
+        </Logo>
+        <Search>
+          <div>
+            <input type="text" placeholder="Search" />
+          </div>
+          <SearchIcon>
+            <img src="/images/search-icon.svg" alt="home-logo" />
+          </SearchIcon>
+        </Search>
+        <Nav>
+          <NavListWrap>
+            <NavList>
+              <a>
+                <img src="/images/nav-home.svg" alt="" />
+                <span>Home</span >
+              </a>              
+            </NavList>
+          </NavListWrap>
+        </Nav> 
+      </Content> 
+    </Container>
+  );
 };
 
 const Container = styled.div`
-    background-color:white;
-    border-bottom: 1px solid rgba(0, 0, 0, 0.08);
-    left: 0;
-    padding: 0 24px;
-    position: fixed;
-    top: 0;
-    width: 100vw;
-    z-index: 100;   
+  background-color: white;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+  left: 0;
+  padding: 0 24px;
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  z-index: 100;
 `;
 
 const Content = styled.div`
-    display: flex;
-    align-items:center;
-    margin: 0 auto;
-    min-height: 100%;
-    max-width: 1128px;
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+  min-height: 100%;
+  max-width: 1128px;
 `;
 
 const Logo = styled.span`
@@ -47,42 +57,98 @@ const Logo = styled.span`
 `;
 
 const Search = styled.div`
-    opacity: 1;
-    flex-grow: 1;
-    position: relative;
-    & > div {
-        max-width: 280px;
-        input {
-            border: none;
-            box-shadow: none;
-            background-color: #eef3f8;
-            border-radius: 2px;
-            color: rgba(0, 0, 0, 0.9);
-            width: 280px;
-            padding: 0 8px 0 40px;
-            line-height: 1.75;
-            font-weight: 400;
-            font-size: 14px;
-            height: 34px;
-            border-color: #dce6f1;
-            vertical-align: text-top;
-        }
+  opacity: 1;
+  flex-grow: 1;
+  position: relative;
+  & > div {
+    max-width: 280px;
+    input {
+      border: none;
+      box-shadow: none;
+      background-color: #eef3f8;
+      border-radius: 2px;
+      color: rgba(0, 0, 0, 0.9);
+      width: 280px;
+      padding: 0 8px 0 40px;
+      line-height: 1.75;
+      font-weight: 400;
+      font-size: 14px;
+      height: 34px;
+      border-color: #dce6f1;
+      vertical-align: text-top;
     }
-
+  }
 `;
 
 const SearchIcon = styled.div`
-    width: 48px;
-    position: absolute;
-    z-index: 1;
-    top: 10px;
-    left: 2px;
-    border-radius: 0 2px 2px 0;
-    margin: 0;
-    pointer-events: none;
+  width: 48px;
+  position: absolute;
+  z-index: 1;
+  top: 10px;
+  left: 2px;
+  border-radius: 0 2px 2px 0;
+  margin: 0;
+  pointer-events: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Nav = styled.nav`
+  margin-left: auto;
+  /* display: flex; */
+  display: block;
+  @media (max-width: 768px) {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    background: white;
+    width: 100%;
+  }
+`;
+
+const NavListWrap = styled.ul`
     display: flex;
-    justify-content: center;
+    flex-wrap: nowrap;
+    list-style-type: none;
+`;
+
+const NavList = styled.li`
+    display: flex;
     align-items: center;
+    a {
+        align-items: center;
+        background: transparent;
+        display: flex;
+        flex-direction: column;
+        font-size: 12px;
+        font-weight: 400;
+        justify-content: center;
+        line-height: 1.5;
+        min-height: 42px;
+        min-width: 80px;
+        position: relative;
+        text-decoration: none;
+
+        span {
+            color: rgba(0, 0, 0, 0.6);
+            display: flex;
+            align-items: center;
+        }
+
+        @media (max-width: 768px) {
+            min-width: 70px
+        }
+    }
+
+    &:hover,
+    &:active {
+        a {
+            span {
+                color: rgba(0, 0, 0, 0.9);
+            }
+        }
+    }
 `;
 
 export default Header;
