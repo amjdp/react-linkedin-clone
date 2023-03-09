3a5bcca049e0c0edf611ea8011525879bc0a6a94
amjdp <athiramj123@gmail.com>
2023-03-09 11:15:43 +0530

    navlist completed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -60,6 +60,10 @@ const Header = (props) => {
                 <span>Me</span>
                 <img src="/images/down-icon.svg" alt="" />
               </a>
+
+              <SignOut>
+                <a>Sign Out</a>
+              </SignOut>
             </User>
            
             <Work>
@@ -206,6 +210,19 @@ const NavList = styled.li`
   }
 `;
 
+const SignOut = styled.div`
+  position: absolute;
+  top:45px;
+  background: white;
+  border-radius: 0 0 5px 5px;
+  width: 100px;
+  height: 40px;
+  font-size: 16px;
+  transition-duration: 167ms;
+  text-align: center;
+  display:none;
+`;
+
 const User = styled(NavList)`
   a > svg {
     width: 24px;
@@ -220,10 +237,19 @@ const User = styled(NavList)`
     display: flex;
     align-items: center;
   }
+  &:hover {
+    ${SignOut} {
+      align-items: center;
+      display: flex;
+      justify-content: center;
+    }
+  }
 `;
 
 const Work = styled(User)`
 
 `;
+
+
 
 export default Header;
